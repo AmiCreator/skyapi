7faec861ff6e8b823bb633eef5fec76957acab5b
Mai <99476125+AmiCreator@users.noreply.github.com>
2025-04-26 17:32:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
+import os
+import codecs
+
 from setuptools import setup, find_packages
 
+
+VERSION = '1.2.0'
+DESCRIPTION = 'Async client for Sky mandatory-subscription service'
+
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as file:
+    long_description = file.read()
+
+
 setup(
-    name="skyapi",
-    version="0.1.0",
-    description="Async client for Sky mandatory-subscription service",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    author="SkyManager",
+    name='skyapi',
+    version=VERSION,
+    author='Ami',
+    author_email='<ami.dev.tg@gmail.com>',
+    description=DESCRIPTION,
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     packages=find_packages(),
-    install_requires=["aiohttp", "cachetools"],
-    python_requires=">=3.8"
+    install_requires=['aiohttp', 'cachetools'],
+    keywords=['python', 'SkyManager', 'async', 'asyncio', 'cache'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Unix',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    url='https://github.com/AmiCreator/skyapi',
+    project_urls={
+        'Homepage': 'https://github.com/AmiCreator/skyapi',
+        'Bug Tracker': 'https://github.com/AmiCreator/skyapi/issues',
+    },
 )
